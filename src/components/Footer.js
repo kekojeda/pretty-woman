70792b92928915910a667052fb7d1f9c639d04ef
Julieta Bachur Cicconi <julietabachur@hotmail.com>
2022-12-26 12:35:50 -0300

    cambio header, footer, agrego páginas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,70 +1,61 @@
 import React from "react";
+import { FaFacebookSquare } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { FaWhatsapp } from 'react-icons/fa';
+import { MdLocationOn } from 'react-icons/md';
+import { BsClock } from 'react-icons/bs';
 
 
 function Footer() {
   return (
    
-<>
-<div className="container">
-  <footer className="py-5">
-    <div className="row">
-      <div className="col-2">
-        <h5>Section</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div className="col-2">
-        <h5>Section</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div className="col-2">
-        <h5>Section</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
-        </ul>
-      </div>
-
-      <div className="col-4 offset-1">
-        <form>
-          <h5>Subscribe to our newsletter</h5>
-          <p>Monthly digest of whats new and exciting from us.</p>
-          <div className="d-flex w-100 gap-2">
-            <label for="newsletter1" className="visually-hidden">Email address</label>
-            <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
-            <button className="btn btn-primary" type="button">Subscribe</button>
+    <>
+      <div className="container">
+        <footer className="py-5">
+          <div className="row">
+            <div className="col-4">
+              <h5>Seguinos</h5>
+              <ul className="nav flex-column align-items-center">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted"><FaFacebookSquare/></a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted"><FiInstagram/></a></li>                
+              </ul>
+            </div>
+            <div className="col-4">
+              <h5>Contactanos</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted"><FaWhatsapp/> XXXXXXXX</a></li>
+              </ul>
+            </div>
+            <div className="col-4">
+              <h5>Te esperamos...</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted"><MdLocationOn/> Dirección: xxxxxxx</a></li>
+                <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted"><BsClock/> Horarios: xx a xx hs</a></li>
+              </ul>
+            </div>
+            {/* <div className="col-4 offset-1">
+              <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of whats new and exciting from us.</p>
+                <div className="d-flex w-100 gap-2">
+                  <label for="newsletter1" className="visually-hidden">Email address</label>
+                  <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
+                  <button className="btn btn-primary" type="button">Subscribe</button>
+                </div>
+              </form>
+            </div> */}
           </div>
-        </form>
+          <div className="d-flex justify-content-between py-4 my-4 border-top">
+            <p>&copy; 2022 Company, Inc. All rights reserved.</p>
+            <ul className="list-unstyled d-flex">
+              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"></svg></a></li>
+              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"></svg></a></li>
+              <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"></svg></a></li>
+            </ul>
+          </div>
+        </footer>
       </div>
-    </div>
-
-    <div className="d-flex justify-content-between py-4 my-4 border-top">
-      <p>&copy; 2022 Company, Inc. All rights reserved.</p>
-      <ul className="list-unstyled d-flex">
-        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"></svg></a></li>
-        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"></svg></a></li>
-        <li className="ms-3"><a className="link-dark" href="#"><svg className="bi" width="24" height="24"></svg></a></li>
-      </ul>
-    </div>
-  </footer>
-  </div>
-  </>
+    </>
   )
   }
 
---
+++ src/components/Servicios.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const servicios = () => {
+  return (
+
+    <div>
+        <h1>Servicios Blooma</h1>
+        <p>Ofrecemos más de 20 servicios dedicados exclusivamente a vos</p>
+
+
+    </div>
+  )
+}
+
+export default servicios
